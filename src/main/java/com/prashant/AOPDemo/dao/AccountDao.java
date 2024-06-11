2faffd6b6ee1d94f9024890008daceab4e9086b0
Pt1234567 <tripathiprashant45678@gmail.com>
2024-06-11 22:31:17 +0530

    dsdvs

---
+++ src/main/java/com/prashant/AOPDemo/dao/AccountDao.java
@@ -0,0 +1,11 @@
+package com.prashant.AOPDemo.dao;
+
+import com.prashant.AOPDemo.Account;
+
+public interface AccountDao {
+
+    void addAccount(Account account,boolean Flag);
+
+    void SayHello();
+
+}
